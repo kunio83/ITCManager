dc6f12ad6b8e8a96fd920e07cd9ec8456dca7af5
kunio83 <marcosflorentin1@hotmail.com>
2016-04-05 20:24:28 -0300

    Modificando el proyecto de Servicios

--- Services/Properties/AssemblyInfo.cs
+++ Services/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1613b868-ebf8-4dee-a67a-f4a09ae48aa8")]
+[assembly: Guid("41d45c92-5a26-4bc2-9b4c-2877c92eaa51")]
 
 // Version information for an assembly consists of the following four values:
 //
