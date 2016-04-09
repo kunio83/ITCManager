28355be7c7fafbaf6fa0cbf732c9a37ba594a582
kunio83 <marcosflorentin1@hotmail.com>
2016-04-09 14:47:05 -0300

    Incluyo los que faltaban

---
+++ ITCManager.Core.DataAccess/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITCManager.Core.DataAccess
+{
+    public interface IRepository<T>
+    {
+        T GetById(int id);
+        IQueryable<T> GetAll();
+        void Edit(T entity);
+        void Insert(T entity);
+        void Delete(T entity);
+    }
+}
